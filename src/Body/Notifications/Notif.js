cb1dea5aa9fd99abeab8348f9f31a50211ea5cc4
Sepideh <108736695+sepidehtajdini@users.noreply.github.com>
2024-02-06 17:18:36 +0330

    Create Notif.js

---
+++ src/Body/Notifications/Notif.js
@@ -0,0 +1,64 @@
+import { useContext } from "react";
+import PostContext from "../../PostContext";
+import Button from "../../Button/Button";
+
+export default function Notif({ person, time, img, noteType, comment, likeCount }) {
+    const { theme } = useContext(PostContext);
+    let note;
+    if (noteType === "posted") {
+        note = 'posted a photo'
+    }
+    if (noteType === "liked comment") {
+        note = "liked your comment:"
+    }
+    if (noteType === "liked photo") {
+        note = `and ${likeCount} others liked your photo`
+    }
+    if (noteType === "commented") {
+        note = "commented on your post:"
+    }
+    if (noteType === "followed") {
+        note = "started following you"
+    }
+    if (noteType === "requested") {
+        note = "requested to follow you"
+    }
+    return (
+        <div className={theme === "light"
+            ? "light_div_items div_items"
+            : "dark_div_items div_items"}>
+            <div className="items_left">
+                <div>
+                    <img src={Object.values(person.src).toString()} className="creator_img"
+                        alt={person.id} />
+                </div>
+                {person.id} {note}
+                {comment ?
+                    <div className="ellipsis">{comment}</div>
+                    : null
+                }
+                <div className="msgFont" style={{ marginLeft: 7 + "px" }}>{time}</div>
+            </div>
+            <div className="items_right">
+                {noteType === "requested" ?
+                    <div style={{ display: "flex", gap: 10 + "px" }}>
+                        <Button
+                            btnClass="blueBtn"
+                            txt="Accept" />
+                        <Button
+                            btnClass="redBtn"
+                            txt="Decline" />
+                    </div>
+                    : null}
+                {img ?
+                    <img
+                        loading="lazy"
+                        src={img}
+                        className="notifications_post_img"
+                        alt="flower" />
+                    : null
+                }
+            </div>
+        </div>
+    )
+}
